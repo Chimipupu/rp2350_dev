8a1a998538be319641aee215764bb9c83720ab0a
Chimipupu <chiminyanyan@gmail.com>
2025-06-12 16:11:36 +0900

    レジスタを8/16/32bitでR/Wするマクロを実装

--- src/rp2350_dev/app_main.h
+++ src/rp2350_dev/app_main.h
@@ -12,6 +12,20 @@
 #include "hardware/clocks.h"
 #include "pico/multicore.h"
 
+// レジスタを8/16/32bitでR/Wするマクロ
+#define REG_READ_BYTE(base, offset)   (*(volatile uint8_t  *)((base) + (offset)))
+#define REG_READ_WORD(base, offset)   (*(volatile uint16_t *)((base) + (offset)))
+#define REG_READ_DWORD(base, offset)  (*(volatile uint32_t *)((base) + (offset)))
+#define REG_WRITE_BYTE(base, offset, val)   (*(volatile uint8_t  *)((base) + (offset)) = (val))
+#define REG_WRITE_WORD(base, offset, val)   (*(volatile uint16_t *)((base) + (offset)) = (val))
+#define REG_WRITE_DWORD(base, offset, val)  (*(volatile uint32_t *)((base) + (offset)) = (val))
+
+// レジスタビット操作
+#define REG_BIT_SET(reg, bit)   ((reg) |=  (1UL << (bit))) // レジスタのビットをセット
+#define REG_BIT_CLR(reg, bit)   ((reg) &= ~(1UL << (bit))) // レジスタのビットをクリア
+#define REG_BIT_TGL(reg, bit)   ((reg) ^=  (1UL << (bit))) // レジスタのビットをトグル
+#define REG_BIT_CHK(reg, bit)   ((reg) &   (1UL << (bit))) // レジスタのビットチェック
+
 // 四則演算の回数（整数、float,double）100万回
 #define TEST_LOOP_CNT 1000000
 
